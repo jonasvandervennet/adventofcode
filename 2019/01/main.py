6ce1ce87ccf6aa17bc7e8f5d7961326baa619eac
Jonas Vander Vennet <jonas.vdv@hotmail.com>
2019-12-02 08:57:18 +0100

    2019 day 1

---
+++ 2019/01/main.py
@@ -0,0 +1,27 @@
+"""
+Written by: Jonas Vander Vennet
+on: 2019/12/02
+Answer: 3330521
+"""
+
+def mass_to_fuel(mass):
+    return int(mass / 3) - 2
+
+
+def main():
+    with open('test.txt') as ifp:
+        test_inputs = ifp.readlines()
+    # cast read values to int
+    test_inputs = [[int(val) for val in line.split()] for line in test_inputs]
+    for mass, answer in test_inputs:
+        assert(mass_to_fuel(mass) == answer)
+
+    with open('input.txt') as ifp:
+        inputs = ifp.readlines()
+    # cast read values to int
+    inputs = [int(val) for val in inputs]
+    # Sum of all fuel needs
+    print(sum([mass_to_fuel(mass) for mass in inputs]))
+
+if __name__ == '__main__':
+    main()
