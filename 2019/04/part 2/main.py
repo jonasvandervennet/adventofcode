24d9566444a872502544a34122a78a16d71d00cf
Jonas Vander Vennet <jonas.vdv@hotmail.com>
2019-12-04 08:12:14 +0100

    2019 day 4

---
+++ 2019/04/part 2/main.py
@@ -0,0 +1,54 @@
+"""
+Written by: Jonas Vander Vennet
+on: 2019/12/04
+Answer: 699
+"""
+
+
+def is_valid_password(password: str):
+    repetition_size = 1
+    repetition = False
+    previous = password[0]
+    for letter in password[1:]:
+        if letter == previous:
+            repetition_size += 1
+        else:
+            if repetition_size == 2:
+                repetition = True
+            repetition_size = 1
+        if int(letter) < int(previous):
+            return False
+        previous = letter
+
+    if repetition_size == 2:
+        repetition = True
+    return repetition
+
+
+def crack_password(start, end):
+    solutions = []
+    for i in range(start, end + 1):
+        if is_valid_password(str(i)):
+            solutions.append(i)
+    return solutions
+
+
+def main():
+    test_values = [
+        ('112233', True),
+        ('123444', False),
+        ('111122', True)
+    ]
+    for pw, valid in test_values:
+        assert(is_valid_password(pw) == valid)
+
+    with open('../input.txt') as ifp:
+        start, end = ifp.readline().split('-')
+    start = int(start)
+    end = int(end)
+    passwords = crack_password(start, end)
+    print(len(passwords))
+
+
+if __name__ == '__main__':
+    main()
